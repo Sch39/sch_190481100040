95fa89a0ca9983089104879f77e43ae537165387
Sch 39 <sch40071807@gmail.com>
2020-08-20 06:57:02 +0700

    tambahkan file index.html

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+$y= 29;
+$x= 6;
+$z= 4;
+$a= $y+$x+$z;
+$namaQ = "Sukron Ch";
+$nama_depan = "Sukron";
+$nama_belakang = "Chafidhi";
+echo "hello guyss, $namaQ atau Sch_$a";
+echo $nama_depan." ".$nama_belakang;
+// var_dump("hello guyss");
+var_dump(1==5);
+var_dump(1=="1");
+var_dump(1==="1");
+
+ ?>
